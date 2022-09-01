ad5152687078523e2b8efb84128de5415d811b83
HarshSonawane <sonawaneharshwardhan@gmail.com>
2022-09-01 10:19:20 +0530

    update

--- core/settings.py
+++ core/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'rest_framework'
+    'rest_framework',
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
